0027da2efb389d6806e8f72c5af5c92e94037b97
unknown <mustufabaigwastaken@gmail.com>
2026-01-10 21:16:18 +0530

    Added Sphere with Lambert Diffuse Shading

--- PathTracing/src/Renderer.cpp
+++ PathTracing/src/Renderer.cpp
@@ -1,6 +1,20 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+
+namespace Utils {
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage) {
@@ -24,7 +38,10 @@ void Renderer::Render()
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
 
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth() ,(float)y / (float)m_FinalImage->GetHeight() };
-			m_FinalImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+			coord = coord * 2.0f - 1.0f;
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_FinalImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
@@ -32,9 +49,31 @@ void Renderer::Render()
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
-	return 0xff000000 | g<<8 | r;
+
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	float radius = 0.5f;
+
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant < 0.0f)
+		return glm::vec4(0,0,0,1);
+
+	float t = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+	glm::vec3 hitPoint = rayOrigin + rayDirection * t;
+	glm::vec3 normal = glm::normalize(hitPoint);
+
+	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+	float intensity = glm::max(glm::dot(normal, -lightDir), 0.0f);
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= intensity;
+	return glm::vec4(sphereColor, 1.0f);
 }
